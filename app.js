2e3d43527b28affa35989af356c3c38e6fc1fe87
James Albert <James.Albert1986@outlook.com>
2022-05-03 13:59:23 -0400

    finished project

---
+++ app.js
@@ -0,0 +1,66 @@
+const body = document.querySelector('body');
+
+// Functions are triggered by onclick events in html
+
+function displayGallery() {
+    let modalHtml = `
+        <div class='modal-overlay'>
+            <div class='modal-card'>
+              <div class='modal-content'>
+                <button class='modal-close' onclick='event.target.parentNode.parentNode.parentNode.remove()'>X</button>
+                <a href='projects/interactive-photo-gallery/index.html'><img class='modal-img' src='img/gallery-img.png'></a>
+                <p class='info'>A photo gallery using CSS grid for layout and Javascript for
+                searchablility</p>
+              </div>
+            </div>
+        </div>
+    `;
+    body.insertAdjacentHTML('afterbegin', modalHtml)
+}
+
+function displayGame() {
+  let modalHtml = `
+      <div class='modal-overlay'>
+          <div class='modal-card'>
+            <div class='modal-content'>
+              <button class='modal-close' onclick='event.target.parentNode.parentNode.parentNode.remove()'>X</button>
+              <a href='projects/game-show-app/index.html'><img class='modal-img' src='img/game-img.png'></a>
+              <p class='info'>A word guessing game using Javascript. There are several movie phrases to guess with 5 lives.</p>
+            </div>
+          </div>
+      </div>
+  `;
+  body.insertAdjacentHTML('afterbegin', modalHtml)
+}
+
+function displayDashboard() {
+  let modalHtml = `
+      <div class='modal-overlay'>
+          <div class='modal-card'>
+            <div class='modal-content'>
+              <button class='modal-close' onclick='event.target.parentNode.parentNode.parentNode.remove()'>X</button>
+              <a href='projects/webapp-dashboard/index.html'><img class='modal-img' src='img/dashboard-img.png'></a>
+              <p class='info'>A dashboard displaying user data, complete with form. Charts are made using chart.js and main traffic chart can toggle
+              between traffic times. Layout is made using CSS grid and flexbox.</p>
+            </div>
+          </div>
+      </div>
+  `;
+  body.insertAdjacentHTML('afterbegin', modalHtml)
+}
+
+function displayDirectory() {
+  let modalHtml = `
+      <div class='modal-overlay'>
+          <div class='modal-card'>
+            <div class='modal-content'>
+              <button class='modal-close' onclick='event.target.parentNode.parentNode.parentNode.remove()'>X</button>
+              <a href='projects/employee-directory/index.html'><img class='modal-img' src='img/directory-img.png'></a>
+              <p class='info'>A directory using fetch API. Javascript is used to display the random data and create
+              the modal displays.</p>
+            </div>
+          </div>
+      </div>
+  `;
+  body.insertAdjacentHTML('afterbegin', modalHtml)
+}
